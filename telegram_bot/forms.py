c270499885064dda63974ff847bbe94ce61d6bd8
swtomas <abramov.vsevolod@gmail.com>
2025-07-05 12:14:34 +0000

    development moved to dev branch

--- telegram_bot/forms.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
-
-router = Router()
-
-class Form(StatesGroup):
-   pass #soon
