7b631ef6b5533b9f89de00d692508434597c0e3d
swtomas <abramov.vsevolod@gmail.com>
2025-07-05 13:03:05 +0000

    development moved to dev branch

--- telegram_bot/callbacks.py
+++ telegram_bot/callbacks.py
@@ -5,10 +5,10 @@ from aiogram.fsm.state import StatesGroup, State
 import dict
 import buttons
 
-class From(StatesGroup):
+class Form(StatesGroup):
  chatgpt=State()
  chatgptsearch=State()
- chatgpt3o = State()
+ chatgpto3 = State()
  
 
 router = Router()
@@ -21,8 +21,23 @@ async def ru_start(callback: CallbackQuery):
   print(e) 
 
 @router.callback_query(F.data == "chatgpt")
-async def chatgpt(callback: CallbackQuery):
+async def chatgpt(callback: CallbackQuery, state: FSMContext):
  try:
   await callback.message.edit_text(text=f"{callback.from_user.first_name}, выбран ChatGPT 4.1. Напишите ваш запрос:")
+  await state.set_state(Form.chatgpt)
  except Exception as e:
   print(e)  
+
+@router.callback_query(F.data == "chatgptsearch")
+async def chatgpt(callback: CallbackQuery):
+ try:
+  await callback.message.edit_text(text=f"{callback.from_user.first_name}, выбран ChatGPT 4o mini Search. Напишите ваш запрос:")
+ except Exception as e:
+  print(e)  
+
+@router.callback_query(F.data == "chatgpto3")
+async def chatgpt(callback: CallbackQuery):
+ try:
+  await callback.message.edit_text(text=f"{callback.from_user.first_name}, выбран ChatGPT o3. Напишите ваш запрос:")
+ except Exception as e:
+  print(e)      
--- telegram_bot/commands.py
+++ telegram_bot/commands.py
@@ -10,3 +10,7 @@ router = Router()
 async def start(message: Message):
     await message.answer(text=f"Привет, {message.from_user.first_name}!\n{dict.menu()}", parse_mode="HTML", reply_markup=buttons.ru_menu()) # type: ignore
     return True
+
+@router.message(Command("check"))
+async def check(message: Message):
+    await message.answer_sticker(sticker="CAACAgQAAxkBAAEPyz5oSbCtp9q8niGGvhmbVY0KnusvpwACVwwAAtoI6FBmuh4vtd-VqDYE")
